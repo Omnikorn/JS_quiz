da3306104bff187924a158101b97e4432a45ff68
Harith Albadry <harith.albadry@gmail.com>
2021-03-03 14:36:21 +0000

    basic file structres including basic css styling

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+// questions
+// this might  need to go in an array 
+q1 {
+    text:" where is the eiffel tower located ?";
+    options: ["Paris", "London", "Moscow", "Berlin"];
+    correctAnswer:"Paris"
+};
+
+q2 {
+    text:"Where is the Beetham tower located ?";
+    options: ["london", "Manchester", "New Castle", "Hull"];
+    correctAnswer:"Manchester";
+}
