89f2701865ba0035936297b26c5d0b33d668b509
Harith Albadry <harith.albadry@gmail.com>
2021-03-04 15:25:49 +0000

    added img to header

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,25 @@
 // questions
 // this might  need to go in an array 
-q1 {
+questions=[
+    q1 {
     text:" where is the eiffel tower located ?";
     options: ["Paris", "London", "Moscow", "Berlin"];
     correctAnswer:"Paris"
-};
+},
 
-q2 {
+    q2 {
     text:"Where is the Beetham tower located ?";
     options: ["london", "Manchester", "New Castle", "Hull"];
     correctAnswer:"Manchester";
+},
+];
+
+for (var i=0; i<question.length; i++ ) {
+    let askQuestion=getElementById("questionHere");
+    askQuestion.appendChild(questions[i].text);
+    
+    for (var j=0, j<questions[i].options[j], j++) {
+        
+    }
+
 }
